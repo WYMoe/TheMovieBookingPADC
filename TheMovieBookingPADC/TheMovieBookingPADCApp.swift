6c7698524b84a400599007889a79011bcb839471
WYMoe <waiyan.mdycode@gmail.com>
2023-05-04 18:51:52 +0630

    Initial Commit

---
+++ TheMovieBookingPADC/TheMovieBookingPADCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieBookingPADCApp.swift
+//  TheMovieBookingPADC
+//
+//  Created by Wai Yan Moe on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieBookingPADCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
